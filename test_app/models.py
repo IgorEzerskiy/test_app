1dded838817167f1fefdf395979dc23013d2138b
Ezerskiy Igor <ezerskiyigor2000@gmail.com>
2023-08-30 18:59:50 +0300

    Added db and models

--- test_app/models.py
+++ test_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from random import randint
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    probation = models.DecimalField(
+        max_digits=2,
+        decimal_places=1
+    )
+    position = models.CharField(max_length=100)
+
+
+class Order(models.Model):
+    task_id = models.CharField(max_length=8)
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=550)
+    employee = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='orders'
+    )
+
+    def save(self, **kwargs):
+        self.task_id = ''.join([str(randint(0, 8)) for number in range(0, 8)])
+        super().save(**kwargs)
--- test_task/settings.py
+++ test_task/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'test_app'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'test_task.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test_task_db',
+        'USER': 'test_task_db',
+        'PASSWORD': '1111',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -112,6 +117,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = 'test_app.User'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
